b492b73d55fcb664af272095a4208e90f6458766
mingshing <mingshing.kuo@thekono.com>
2017-11-17 11:50:50 +0800

    Initial Commit

---
+++ KonoWorker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KonoWorker
+//
+//  Created by kuokuo on 2017/11/17.
+//  Copyright © 2017年 Kono. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
